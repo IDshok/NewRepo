a4b201d36e027be24ead99bebcfb8cd20b8cf3c4
Ace <Ace@DESKTOP-2Q68R61>
2024-10-11 08:36:21 +0300

    lastmod

--- Ddtatest/Program.cs
+++ Ddtatest/Program.cs
@@ -13,18 +13,18 @@ builder.Services.AddApplicationCors();
 builder.Services.AddAppVersioning();
 builder.Services.AddAppSwagger(mainSettings, swaggerSettings);
 builder.Services.AddAppControllerAndViews();
-//builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseAppSwagger();
 app.UseAppControllerAndViews();
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
 
 
